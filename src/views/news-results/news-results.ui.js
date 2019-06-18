a619f63bd6eaf53936f372cfb370ad7bd6a7019a
Kaiz Hudda <kaiz.hudda@gmail.com>
2019-06-18 20:59:23 +1200

    Added points, comments, author and created at dates plus UI styling for the articles items

--- src/views/news-results/news-results.ui.js
+++ src/views/news-results/news-results.ui.js
@@ -1,5 +1,5 @@
 import React, { useEffect } from 'react';
-
+import moment from 'moment';
 import './news-results.scss';
 
 const NewsResults = ({ loadTest, test, hits }) => {
@@ -7,15 +7,25 @@ const NewsResults = ({ loadTest, test, hits }) => {
     // Load test action
     loadTest();
   }, [loadTest])
-  
+
   return (
-    <div className="row center-lg center-md">
+    <div className="News-Results row center-lg center-md">
       <div className="col-xs-12 col-sm-12 col-md-8 col-lg-8">
         <h2>News Results</h2>
         <p>{test}</p>
         {
           hits.map((hit, index) => (
-            <h1 key={index}>{hit.title}</h1>
+            <div key={index} className="hit-item">
+              <h3>{hit.title}</h3>
+              <div className="support-hit">
+                <p>{hit.points} points</p>
+                <p>{hit.author}</p>
+                <p>
+                    {moment(hit.created_at).fromNow()}
+                </p>
+                <p>{hit.num_comments} comments</p>
+              </div>
+            </div>
           ))
         }
       </div>
