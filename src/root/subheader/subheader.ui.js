02692406eeba4cdd712abbd4e4d25114335c74ea
Kaiz Hudda <kaiz.hudda@gmail.com>
2019-06-25 18:21:03 +1200

    Added React Dropdown and styling it

--- src/root/subheader/subheader.ui.js
+++ src/root/subheader/subheader.ui.js
@@ -1,14 +1,28 @@
 import React, { useState } from 'react';
+import Dropdown from 'react-dropdown';
+import 'react-dropdown/style.css';
 
 import './subheader.scss';
 
+const options = [
+  { value: 'all', label: 'All' },
+  { value: 'stories', label: 'Stories', className: 'myOptionClassName' },
+  { value: 'comments', label: 'Comments' }
+];
+
 const SubHeader = ({}) => {
+  const defaultOption = options[0]
 
   return (
     <div className="SubHeader row">
       <div className="col-xs-12 col-lg-8 col-lg-offset-2">
         <div className="subheader-content">
-          <p>Sub Header</p>
+          <p>Search</p>
+          <Dropdown 
+            className="tags-dropdown"
+            options={options}
+            value={defaultOption}
+            placeholder="Select an option" />
         </div>
       </div>
     </div>
