fa45916c3b6ddbf787bb43f109d292b2d91fcdf0
Kaiz Hudda <kaiz.hudda@gmail.com>
2019-07-14 16:52:34 +1200

    Connected date range dropdown to redux

--- src/store/actions/news-actions.js
+++ src/store/actions/news-actions.js
@@ -2,14 +2,12 @@ import { FETCH_NEWS_DATA } from './types';
 import newsSearchAPI from '../../apis/news-search';
 import { selectFilters } from '../selectors/index';
 
-import moment from 'moment';
-
 export const fetchNewsData = () => async (dispatch, getState) => {
   const state = getState();
   const filters = selectFilters(state);
   console.log(filters);
-  const created_at = moment().subtract(1, 'days').unix(); // last 24 hours
-  const response = await newsSearchAPI.get(`${filters.relevance}?query=${filters.query}&tags=${filters.tags}&numericFilters=created_at_i>${created_at}`);
+  const created_at = filters.createdAt ? `&numericFilters=created_at_i>${filters.createdAt}` : '';
+  const response = await newsSearchAPI.get(`${filters.relevance}?query=${filters.query}&tags=${filters.tags}${created_at}`);
   console.log(response);
   dispatch({
     type: FETCH_NEWS_DATA,
