117ce4eecb132e4ed21dcb62c9a75c8d0f70308a
Kaiz Hudda <kaiz.hudda@gmail.com>
2019-07-14 20:49:47 +1200

    Styling settings header

--- src/views/settings/settings.ui.js
+++ src/views/settings/settings.ui.js
@@ -5,7 +5,12 @@ const Settings = () => {
   return (
     <div className="Settings row center-lg center-md">
       <div className="col-xs-12 col-sm-12 col-md-8 col-lg-7">
-        Settings
+        <div className="content">
+          <div className="header-area">
+            <h2 className="heading">Settings</h2>
+            <i class="fas fa-chevron-left" />
+          </div>
+        </div>
       </div>
     </div>
   );
