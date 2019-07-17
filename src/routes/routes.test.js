e848ff64a2b5f9fecfece82f1112052a82bcdf5a
Kaiz Hudda <kaiz.hudda@gmail.com>
2019-07-17 20:27:36 +1200

    Tested Routes Component

---
+++ src/routes/routes.test.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import Routes from './index';
+
+let wrapped = shallow(<Routes />);
+
+describe('Routes', () => {
+  it('should render the Routes Component', () => {
+    expect(wrapped).toMatchSnapshot();
+  });
+
+  it('should have 1 route', () => {
+    expect(wrapped.find('Route').length).toBe(1);
+  });
+});
