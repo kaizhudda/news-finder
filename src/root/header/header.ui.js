407c6dca1663f9228c1d87d698e647aeba622e4b
Kaiz Hudda <kaiz.hudda@gmail.com>
2019-08-03 16:32:59 +1200

    Styling changes part 2

--- src/root/header/header.ui.js
+++ src/root/header/header.ui.js
@@ -1,4 +1,5 @@
 import React, { useState, useCallback } from 'react';
+import { Link } from 'react-router-dom';
 import _ from 'lodash';
 import { withRouter } from 'react-router-dom';
 import DarkModeToggle from '../../shared-components/toggle';
@@ -21,7 +22,7 @@ const Header = ({ setQuery, location: { pathname } }) => {
     <div className="Header row">
       <div className="col-xs-12 col-lg-8 col-lg-offset-2">
         <div className="header-content">
-          <h1 className="app-title">HN.</h1>
+          <Link to="/" className="app-title">HN.</Link>
           {
             pathname === '/' &&
             <SearchBar onChange={onSearchChange} value={searchTerm} />
